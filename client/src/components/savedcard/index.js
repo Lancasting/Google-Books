a2c43322a94e19bcd005dc8e7865f75996e55328
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-19 14:17:39 -0600

    adding rel to button for googke links

--- client/src/components/savedcard/index.js
+++ client/src/components/savedcard/index.js
@@ -8,7 +8,7 @@ export const SavedCard = (props) => {
                 <h4 className="card-title">{props.title}</h4>
                 <h5 className="card-title">{props.authors}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{props.description}</h6>
-                <a href={props.link} className="btn btn-primary" target="_blank">View</a>
+                <a href={props.link} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View</a>
                 <button type="button" className="btn btn-danger" onClick={props.handleDelete}>Delete</button>
             </div>
         </div>
--- client/src/components/searchcard/index.js
+++ client/src/components/searchcard/index.js
@@ -11,7 +11,7 @@ export const SearchCard = (props) => {
                 <h6 className="card-subtitle mb-2 text-muted">{props.volumeInfo.description}</h6>
                 <button type="button" className="btn btn-primary" onClick={props.saveBook}>Save</button>
                 {/* <button href={props.volumeInfo} type="button" className="btn btn-primary" target="_blank" rel="noopener noreferrer" onClick={props.viewClick} >View</button> */}
-                <a href={props.volumeInfo.infoLink} className="btn btn-primary" target="_blank">View</a>
+                <a href={props.volumeInfo.infoLink} className="btn btn-primary" target="_blank" rel="noopener noreferrer">View</a>
             </div>
         </div>
     );
