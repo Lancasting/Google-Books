7975209dae2da410980f85061365ff865f914503
Samuel Lancaster <samuellancaster66@gmail.com>
2020-08-18 18:24:42 -0600

    changes to button on search card

--- client/src/components/searchcard/index.js
+++ client/src/components/searchcard/index.js
@@ -7,10 +7,11 @@ export const SearchCard = (props) => {
             <div className="card-body">
                 <h4 className="card-title">{props.volumeInfo.title}</h4>
                 <h5 className="card-title">{props.volumeInfo.authors}</h5>
-                <a href={props.volumeInfo.infoLink} className="card-link" target="_blank" rel="noopener noreferrer">Link: {props.link}</a>
+                {/* <a href={props.volumeInfo.infoLink} className="card-link" target="_blank">Link: {props.link}</a> */}
                 <h6 className="card-subtitle mb-2 text-muted">{props.volumeInfo.description}</h6>
                 <button type="button" className="btn btn-primary" onClick={props.saveBook}>Save</button>
-                <button href={props.volumeInfo} type="button" className="btn btn-primary" target="_blank" rel="noopener noreferrer" onClick={props.viewClick} >View</button>
+                {/* <button href={props.volumeInfo} type="button" className="btn btn-primary" target="_blank" rel="noopener noreferrer" onClick={props.viewClick} >View</button> */}
+                <a href={props.volumeInfo.infoLink} className="btn btn-primary" target="_blank">View</a>
             </div>
         </div>
     );
